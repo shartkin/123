a4be89fe8e6b7fd5d405e7e60b868acd8f797e12
shartkin <154659227+shartkin@users.noreply.github.com>
2025-03-03 18:23:36 +0300

    Create S and p.cpp

---
+++ S and p.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    float p, s;
+    std::cout << "enter the remit of the square : ";
+    std::cin >> p;
+    s = pow(p / 4, 2);
+    cout << "\n s= " << s;
+    return 0;
+}
